5185ab32a3868f621153aa4c9c1010a656b179d3
qMaxFcq <maxzazafc123@gmail.com>
2023-10-30 14:00:12 +0700

    test protec

--- src/components/custom/ProtectedRoute.tsx
+++ src/components/custom/ProtectedRoute.tsx
@@ -1,18 +1,11 @@
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 
-type Props = { children: React.ReactNode };
+type Props = { children: any };
 
 export default function ProtectedRoute({ children }: Props) {
-    const navigate = useNavigate(); // Move this line inside the component
-
     const token = localStorage.getItem("token");
     if (token) {
         return children;
     }
-
-    // You should navigate inside the function, not at the module level
-    navigate('/');
-
-    // You might want to return null or a message here as well
-    return null;
+    return <Navigate to="/" />;
 }
