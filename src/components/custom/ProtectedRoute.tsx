020b736d2d03905e0b11eac20bb9116c89955919
qMaxFcq <maxzazafc123@gmail.com>
2023-10-30 14:05:16 +0700

    test f5

--- src/components/custom/ProtectedRoute.tsx
+++ src/components/custom/ProtectedRoute.tsx
@@ -1,11 +1,17 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 type Props = { children: any };
 
 export default function ProtectedRoute({ children }: Props) {
     const token = localStorage.getItem("token");
-    if (token) {
-        return children;
-    }
-    return <Navigate to="/" />;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!token || token === undefined) {
+            localStorage.removeItem("token");
+            navigate("/");
+        }
+    }, [navigate]);
+    return children;
 }
