47b0b47211544d7d7a0342172a43e0b54a682d14
qMaxFcq <maxzazafc123@gmail.com>
2023-10-30 13:51:31 +0700

    test

--- src/components/custom/ProtectedRoute.tsx
+++ src/components/custom/ProtectedRoute.tsx
@@ -1,11 +1,18 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-type Props = { children: any };
+type Props = { children: React.ReactNode };
 
 export default function ProtectedRoute({ children }: Props) {
+    const navigate = useNavigate(); // Move this line inside the component
+
     const token = localStorage.getItem("token");
     if (token) {
         return children;
     }
-    return <Navigate to="/" />;
+
+    // You should navigate inside the function, not at the module level
+    navigate('/');
+
+    // You might want to return null or a message here as well
+    return null;
 }
--- src/components/custom/Redirect.tsx
+++ src/components/custom/Redirect.tsx
@@ -1,12 +1,19 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-type Props = { children: any };
+type Props = { children: React.ReactNode };
 
 export default function Redirect({ children }: Props) {
     const token = localStorage.getItem("token");
-    if (!token || token === undefined) {
-        localStorage.removeItem("token");
-        return children;
-    }
-    return <Navigate to="/home" />;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!token || token === undefined) {
+            localStorage.removeItem("token");
+        } else {
+            navigate("/home");
+        }
+    }, [token, navigate]);
+
+    return children;
 }
