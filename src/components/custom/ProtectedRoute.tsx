631162f9ce05dc02be2ff4d811f39a5edfb2cc14
qMaxFcq <maxzazafc123@gmail.com>
2023-10-30 14:44:27 +0700

    test f5 5

--- src/components/custom/ProtectedRoute.tsx
+++ src/components/custom/ProtectedRoute.tsx
@@ -1,17 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
-type Props = { children: any };
+type Props = { children: React.ReactNode };
 
-export default function ProtectedRoute({ children }: Props) {
+export default function Redirect({ children }: Props) {
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (!token || token === undefined) {
-            localStorage.removeItem("token");
-            navigate("/");
+        if (token && token !== undefined) {
+            // ถ้ามี Token ให้นำทางไปยังหน้าหลัก ("/home")
+            navigate("/home");
+        } else {
+            // ถ้าไม่มี Token ให้นำทางไปยังหน้า Login หรือหน้าที่ใช้ในการล็อกอิน
+            navigate("/login"); // แก้ไขเส้นทางตามที่คุณต้องการ
         }
-    }, [navigate]);
+    }, [token, navigate]);
+
     return children;
 }
